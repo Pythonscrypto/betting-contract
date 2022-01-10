f3f4d0f02b8f1c1ab1aa4e3b863469d9b2d935df
IntegralTeam <repo@sfxdx.ru>
2022-01-10 14:34:25 +0400

    changed deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,14 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+require('dotenv').config();
+
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const Betting = await hre.ethers.getContractFactory("Betting");
+  const betting = await Betting.deploy(process.env.WALLET_ADDRESS, process.env.TOKEN_ADDRESS);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Betting deployed to:", betting.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {

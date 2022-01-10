bb5ba5bf92c7958e8109422400f1501cd63d23cf
IntegralTeam <repo@sfxdx.ru>
2022-01-10 14:34:39 +0400

    changed configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require('solidity-coverage');
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,12 +11,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: {
+      bscTestnet: process.env.API_KEY
+    }
+  }
 };
